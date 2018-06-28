7e01ca2320dac11819be96de7d8da60276e1531e
renejal <40049733+renejal@users.noreply.github.com>
2018-06-28 11:50:09 -0500

    mi primerCommit

---
+++ SCW/DOMINIO/clsCliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCW.DOMINIO
+{
+    class clsCliente
+    {
+        #region atributos
+        String atrNombre;
+        String atrDireccionMac;
+        bool atrEstado;
+        #endregion
+        #region Contructores
+
+        #endregion
+        #region metodos Accesores o get
+        public String getAtrNombre()
+        {
+            return this.atrNombre;
+        }
+        public String getAtrDireccionMac()
+        {
+            return this.atrDireccionMac;
+        }
+        public bool getAtrEstado()
+        {
+            return this.atrEstado;
+        }
+        #endregion
+        #region metodos Mutadores o set
+        public void setAtrNombre(String parNombre)
+        {
+            this.atrNombre = parNombre;
+        }
+        public void setAtrDireccionMac(String parDireccionMac)
+        {
+            this.atrDireccionMac = parDireccionMac;
+        }
+        public void setAtrEstado(bool parEstado)
+        {
+            this.atrEstado = parEstado;
+        }
+        #endregion
+    }
+}
---
+++ SCW/PRESENTACION/index.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SCW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tpg_control = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tpg_Nuevo = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.tpg_control.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tpg_control);
+            this.tabControl.Controls.Add(this.tpg_Nuevo);
+            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(752, 435);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tpg_control
+            // 
+            this.tpg_control.BackColor = System.Drawing.Color.Green;
+            this.tpg_control.Controls.Add(this.label6);
+            this.tpg_control.Controls.Add(this.label5);
+            this.tpg_control.Controls.Add(this.lbel);
+            this.tpg_control.Controls.Add(this.label3);
+            this.tpg_control.Controls.Add(this.panel1);
+            this.tpg_control.Location = new System.Drawing.Point(4, 22);
+            this.tpg_control.Name = "tpg_control";
+            this.tpg_control.Padding = new System.Windows.Forms.Padding(3);
+            this.tpg_control.Size = new System.Drawing.Size(744, 409);
+            this.tpg_control.TabIndex = 0;
+            this.tpg_control.Text = "Control";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGreen;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Location = new System.Drawing.Point(6, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(732, 51);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Location = new System.Drawing.Point(422, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00:00:01";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(267, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(208, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // tpg_Nuevo
+            // 
+            this.tpg_Nuevo.Location = new System.Drawing.Point(4, 22);
+            this.tpg_Nuevo.Name = "tpg_Nuevo";
+            this.tpg_Nuevo.Padding = new System.Windows.Forms.Padding(3);
+            this.tpg_Nuevo.Size = new System.Drawing.Size(744, 409);
+            this.tpg_Nuevo.TabIndex = 1;
+            this.tpg_Nuevo.Text = "Cliente";
+            this.tpg_Nuevo.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(599, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "00:00:01";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombre";
+            // 
+            // lbel
+            // 
+            this.lbel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbel.Location = new System.Drawing.Point(272, 13);
+            this.lbel.Name = "lbel";
+            this.lbel.Size = new System.Drawing.Size(74, 21);
+            this.lbel.TabIndex = 1;
+            this.lbel.Text = "Comenzar";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(432, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 21);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Tiempo";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(609, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 21);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Restante";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 460);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl.ResumeLayout(false);
+            this.tpg_control.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tpg_control;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tpg_Nuevo;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
